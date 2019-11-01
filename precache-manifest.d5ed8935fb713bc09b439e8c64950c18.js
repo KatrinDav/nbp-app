b75d7f0e275c0b96d3435fe24baf4b84c81fbb31
Kasia Dav <k.dawidowicz2016@gmail.com>
2019-11-01 13:05:37 +0100

    Updates

---
+++ precache-manifest.d5ed8935fb713bc09b439e8c64950c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a662206686999ee02e12f0df0fa5653c",
+    "url": "/nbp-app/index.html"
+  },
+  {
+    "revision": "46ea4e8e152a27f03a3b",
+    "url": "/nbp-app/static/css/main.3e1cd01a.chunk.css"
+  },
+  {
+    "revision": "b6867c7edd76bdff2ca4",
+    "url": "/nbp-app/static/js/2.1d2b1bbe.chunk.js"
+  },
+  {
+    "revision": "46ea4e8e152a27f03a3b",
+    "url": "/nbp-app/static/js/main.7be9cc5c.chunk.js"
+  },
+  {
+    "revision": "01d5848f50ddca2d164f",
+    "url": "/nbp-app/static/js/runtime-main.a37ddacd.js"
+  },
+  {
+    "revision": "faec2c394bd03d2d72e3b6e31c04d614",
+    "url": "/nbp-app/static/media/foto1.faec2c39.jpg"
+  }
+]);
